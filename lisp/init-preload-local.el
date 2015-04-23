fc0fa3c13eafcf947efbd58d85ca07c2bbc3e661
laohyx <laohyx@163.com>
2015-04-23 10:46:46 +0800

    Add smarter-move-beginning-of-line. Fix a yasnippet bug in term

--- lisp/init-preload-local.el
+++ lisp/init-preload-local.el
@@ -128,7 +128,7 @@
 (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
 
 (require 'ido)
-(ido-mode t) ;; only use this line to turn off ido for file names!
+(ido-mode t)
 (setq ido-ignore-buffers '("^ " "*Completions*" "*Shell Command Output*"
                "*Messages*" "Async Shell Command"))
 
@@ -137,7 +137,41 @@
 
 (require-package 'yasnippet)
 (yas-global-mode 1)
+(add-hook 'term-mode-hook (lambda()
+        (setq yas-dont-activate t)))
+
 (require-package 'php-mode)
+(require-package 'python-mode)
+
+
+;; http://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line]
+                'smarter-move-beginning-of-line)
+
 
 (provide 'init-preload-local)
 
